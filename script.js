2d77c82c42db15f24ae81443adc42db97ca2266c
AndyMagwayer <andymagwayer@gmial.com>
2023-09-12 20:11:17 +0500

    feat: added and completed function (20:01)

---
+++ script.js
@@ -0,0 +1,19 @@
+function rain(){
+    let cloud = document.querySelector('.cloud');
+    let e = document.createElement('div');
+
+
+
+    e.classList.add('drop');
+    cloud.appendChild(e);
+
+
+ setTimeout(function(){
+    cloud.removeChild(e)
+ },2000)
+}
+
+
+setInterval(function(){
+    rain()
+},20);
